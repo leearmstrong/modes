dbbf9916b17a5f7fa1fcfb3be685092e5c9ed628
Lee Armstrong <lee@pinkfroot.com>
2024-10-22 12:51:44 +0100

    Magnetic heading fixes

--- pkg/bds/bds60/fields/magnetic_heading.go
+++ pkg/bds/bds60/fields/magnetic_heading.go
@@ -6,15 +6,21 @@ import (
 
 func ReadMagneticHeading(data []byte) (bool, float32) {
 	status := (data[0] & 0x80) != 0
+	heading_sign := data[0] & 0x40 >> 6
 
 	byte1 := data[0] & 0x3F
 	byte2 := data[1] & 0xF0
-	allBits := bitutils.Pack2Bytes(byte1, byte2) >> 4
-	magneticHeading := float32(allBits) * 90.0 / 512.0
+	allBits := float32(bitutils.Pack2Bytes(byte1, byte2) >> 4)
 
-	if (data[0] & 0x40) != 0 {
-		magneticHeading = magneticHeading - 180
+	if heading_sign == 1 {
+		allBits = allBits - 1024
 	}
 
-	return status, magneticHeading
+	magnetic_heading := allBits * 90 / 512
+
+	if magnetic_heading < 0 {
+		magnetic_heading = 360 + magnetic_heading
+	}
+
+	return status, magnetic_heading
 }
