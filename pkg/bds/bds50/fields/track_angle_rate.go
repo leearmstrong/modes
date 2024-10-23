212a59bf6d0c8d9eb5ad1ab644acff0ee662e7ca
Lee Armstrong <lee@pinkfroot.com>
2024-10-23 06:20:27 +0100

    Track Angle Rate slight refactor

--- pkg/bds/bds50/fields/track_angle_rate.go
+++ pkg/bds/bds50/fields/track_angle_rate.go
@@ -4,15 +4,17 @@ import "github.com/twuillemin/modes/pkg/bitutils"
 
 func ReadTrackAngleRate(data []byte) (bool, float32) {
 	status := (data[4] & 0x20) != 0
+	track_sign := (data[4] & 0x10) >> 4
 
 	byte1 := data[4] & 0x0F
 	byte2 := data[5] & 0xF8
-	allBits := bitutils.Pack2Bytes(byte1, byte2) >> 3
-	angleRate := float32(allBits) * 8.0 / 256.0
+	allBits := float32(bitutils.Pack2Bytes(byte1, byte2) >> 3)
 
-	if (data[4] & 0x10) != 0 {
-		angleRate = angleRate - 16
+	if track_sign == 1 {
+		allBits = allBits - 512
 	}
+
+	angleRate := allBits * 8 / 256
 
 	return status, angleRate
 }
