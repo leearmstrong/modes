7e8f1b340b88d88eadff03b5dcdee50aa6125eff
Lee Armstrong <lee@pinkfroot.com>
2024-10-23 06:23:49 +0100

    Fixes for track angle

--- pkg/bds/bds50/fields/true_track_angle.go
+++ pkg/bds/bds50/fields/true_track_angle.go
@@ -6,14 +6,20 @@ import (
 
 func ReadTrueTrackAngle(data []byte) (bool, float32) {
 	status := (data[1] & 0x10) != 0
+	angle_sign := data[1] & 0x08 >> 3
 
 	byte1 := data[1] & 0x07
 	byte2 := data[2] & 0xFE
-	allBits := bitutils.Pack2Bytes(byte1, byte2) >> 1
-	angle := float32(allBits) * 90.0 / 512.0
+	allBits := float32(bitutils.Pack2Bytes(byte1, byte2) >> 1)
 
-	if (data[1] & 0x08) != 0 {
-		angle = angle - 180
+	if angle_sign == 1 {
+		allBits = allBits - 1024
+	}
+
+	angle := allBits * 90 / 512.0
+
+	if angle < 0 {
+		angle = 360 + angle
 	}
 
 	return status, angle
